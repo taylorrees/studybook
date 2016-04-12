5ea5971b2e6e397d27abb703162c30fe81049227
Taylor Rees <hello@taylorrees.co>
2016-04-12 11:44:29 +0100

    Commented the user class.

--- user/User.py
+++ user/User.py
@@ -1,23 +1,53 @@
 class User:
-	
-	loginStatus = False
-	
-	def __init__(self, userID, password, loginStatus = False):
-		self.userID = userID
+
+	def __init__(self, user_id, password, login_status = 0):
+		"""
+		Constructor
+		This class is responsible for creating a user class
+		which will in turn allow the user to login to the
+		system. Using the login_status the user will be
+		granted access to specific parts of the system with
+		the appropriate access levels.
+
+		login_status == 0 == logged out
+		login_status == 1 == lecturer
+		login_status == 2 == student
+
+		"""
+
+		self.user_id = user_id
 		self.password = password
-		self.loginStatus = loginStatus
+		self.login_status = login_status
 
-	def login(userID, password):
-		#values are hardcoded for prototyping
 
-		#lecturer user
-		if (userID == "lecturer" and password=="password"):
-			loginStatus = True
-			return loginStatus
-		elif (userID == "student" and password=="password"):
-			loginStatus = True
-			return loginStatus
+	def login(self, user_id, password):
+		"""
+		Login method
+		Allow a user to login to the system provided
+		their login credentials are correct. Give the
+		user the appropriate access permissions.
 
-	def logout():
-		loginStatus = False
-		return
+		*										*
+		* LOGIN DETAILS HARD CODED TO PROTOTYPE *
+		*										*
+		"""
+
+		if (user_id == "lecturer" and password == "password"):
+			login_status = 1
+			return login_status
+
+		elif (user_id == "student" and password == "password"):
+			login_status = 2
+			return login_status
+
+
+	def logout(self):
+		"""
+		Logout method
+		Set the users login status to the deafult value
+		of zero, thus removing all access rights from the
+		system.
+		"""
+
+		login_status = 0
+		return login_status
