9c546c5e1c4dc1eb0feec9ebc281a6e6cc75c03a
Taylor Rees <hello@taylorrees.co>
2016-04-13 19:19:59 +0100

    Removed accidental test file.

--- test.py
+++
@@ -1,8 +0,0 @@
-import tkinter as tk
-import templates.ui as ui
-
-window = tk.Tk()
-window.title("Studybook | Lesson Viewer")
-img = tk.PhotoImage(file="lesson/img/lorem.gif")
-tk.Label(window, image=img).grid(row=3, column=1)
-window.mainloop()
