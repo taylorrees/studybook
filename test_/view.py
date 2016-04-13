2e24fe89b6df63b00aa91418ddcad2df57497bcd
SuavisLupus <ThomasSweetman31415@gmail.com>
2016-04-13 02:26:45 +0100

    as much as i can
    we have an issue with shelving it will not allow storing class within a
    class which means i cant store the test as it has questions as a
    class...

--- test_/view.py
+++ test_/view.py
@@ -1,13 +1,110 @@
 #Author: Thomas Sweetman
 
+import shelve
+import tkinter as tk
+import templates.ui as ui
+
 class TestView:
 
-	def main(_id):
-		store = shelve.open('test/store')
-		test = store[_id]
-		store.close()
+	def __init__(self, root, test, lesson):
+		self.root = root
+		self.lesson = lesson
+		self.test = test
+		self.current_question = -1
 
-		window = tk.Tk()
-		window.title("Studybook | Test Viewer")
-		app = TestView(window, test)
-		window.mainloop()
+		self.title = ui.title(self.root, text=lesson.name, row=1)
+		self.body = tk.Label(self.root, text='Click next to start the test')
+		self.previous_button = tk.Button(self.root, text="Previous Question", command=self.previous)
+		self.next_button = tk.Button(self.root, text="Next Question", command=self.next)
+		self.complete_button = tk.Button(self.root, text="Submit Test", command=self.complete)
+
+		self.build()
+
+	def previous(self):
+
+		if self.current_question -1 > -1:
+			self.current_question -= 1
+
+		question = self.test.questions[self.current_question]
+		self.title.destroy()
+		self.body.destroy()
+
+		self.detail = tk.Label(self.root, text=question.detail,row=1)
+		self.answer1 = tk.Radiobutton(self.root, text=question.answers[0],column=2,row=4)
+		self.answer2 = tk.Radiobutton(self.root, text=question.answers[1],column=2,row=8)
+		self.answer3 = tk.Radiobutton(self.root, text=question.answers[2],column=6,row=4)
+		self.answer4 = tk.Radiobutton(self.root, text=question.answers[3],column=6,row=8)
+
+		self.build()
+
+	def next(self):
+
+		if self.current_question +1 < len(self.test.questions):
+			self.current_question += 1
+
+		question = self.test.questions[self.current_question]
+		self.title.destroy()
+		self.body.destroy()
+
+		self.detail = tk.Label(self.root, text=question.detail,row=1)
+		self.answer1 = tk.Radiobutton(self.root, text=question.answers[0],column=2,row=4)
+		self.answer2 = tk.Radiobutton(self.root, text=question.answers[1],column=2,row=8)
+		self.answer3 = tk.Radiobutton(self.root, text=question.answers[2],column=6,row=4)
+		self.answer4 = tk.Radiobutton(self.root, text=question.answers[3],column=6,row=8)
+
+		self.build()
+
+	def complete(self):
+
+		self.root.destroy()
+
+	def make_title(self):
+
+		ui.margin_y(self.root, px=20, row=0)
+		self.title
+		ui.margin_y(self.root, px=2, row=2)
+
+	def make_body(self):
+
+		self.body.grid(row=3)
+
+	def make_nav(self):
+
+		padx=20
+		ui.margin_y(self.root, px=20, row=4)
+
+		if self.current_question > 0:
+			self.previous_button.grid(sticky=tk.W, row=5, padx=padx)
+		else:
+			self.previous_button.grid_forget()
+
+		self.next_button.grid(sticky=tk.E, row =5, padx=padx)
+
+		if self.current_question == (len(self.test.questions) - 1):
+			self.next_button.grid_forget()
+			self.complete_button.grid(sticky=tk.E, row=5, padx=padx)
+		else:
+			self.complete_button.grid_forget()
+			self.next_button(sticky=tk.E, row=5, padx=padx)
+
+		ui.margin_y(self.root,px=0,row=8)
+
+	def build(self):
+
+		self.make_title()
+		self.make_body()
+		self.make_nav()
+
+
+def main(_id):
+
+	store = shelve.open('lesson/store')
+	lesson = store[_id]
+	store.close()
+
+	test = lesson.test
+
+	window = tk.Tk()
+	window.title("Studybook | Test Viewer")
+	app = TestView(window, test, lesson)
+	window.mainloop()
--- test_list/view.py
+++ test_list/view.py
@@ -1,11 +1,43 @@
-#Author: Thomas Sweetman
+# Author: Thomas Sweetman
 
-#class TestView:
+import tkinter as tk
+import templates.ui as ui
+import test_.view as test_view
+import shelve
 
 def main():
-	
-	window = tk.Tk()
-	window.title('Studybook | Create Lesson')
-	app = TestView(window)
-	window.grid_columnconfigure(0, weight=1)
-	window.mainloop()
+
+    row = 0
+    window = tk.Tk()
+    window.title("Studybook | Test List")
+
+    store = shelve.open('lesson/store')
+    lessons = [(lesson._id, lesson.name) for lesson in store.values() if lesson.published]
+    store.close()
+
+    ui.margin_y(window, px=20, row=row)
+    row += 1
+
+    ui.title(window, text="Test List", row=row)
+    row += 1
+
+    ui.margin_y(window, px=2, row=row)
+    row += 1
+
+    for lesson in lessons:
+
+    	store = shelve.open('lesson/store')
+    	temp = store[lesson[0]]
+    	store.close()
+
+    	_id     = lesson[0]
+    	name    = lesson[1]
+    	command = lambda _id = _id : test_view.main(_id)
+
+    	ui.pair(window, label_text=name, button_text="Take Test", command=command, row=row)
+    	row += 1
+
+    ui.pair(window, label_text='', button_text="Close", command=window.destroy, row=row)
+    row += 1
+    ui.margin_y(window, px=30, row=row)
+    window.mainloop()
