49f2145c3b2fa419f28e8244e77811c8c27d46c7
Tom Wynne-Owen <tomwo123@gmail.com>
2016-04-13 18:38:44 +0100

    added add_user function

--- user/user.py
+++ user/user.py
@@ -26,25 +26,15 @@ class User:
 		Allow a user to login to the system provided
 		their login credentials are correct. Give the
 		user the appropriate access permissions.
-		*										*
-		* LOGIN DETAILS HARD CODED TO PROTOTYPE *
-		*										*
-
-
-		if (user_id == "lecturer" and password == "password"):
-			self.login_status = 1
-
-		elif (user_id == "student" and password == "password"):
-			self.login_status = 2
-
 		"""
+
 		with open('users.csv') as csvfile:
 			rdr = csv.reader(csvfile)
 			for row in rdr:
 				if (user_id == row[0] and password == row[1]):
 					self.user_id = user_id
 					self.login_status = int(row[2])
-
+		csvfile.close()
 
 
 
@@ -58,3 +48,38 @@ class User:
 
 		login_status = 0
 		return login_status
+
+	def add_user(self, user_id, password, permission=2):
+		"""
+		Add student method.
+		Allows a lecturer to add a student user to the
+		system.
+
+		Permission is login_status (1:lecturer, 2:student)
+
+		Returns True if user has been sucessfully added.
+		Returns False if user already exists.
+		Returns False if login_status is not 1 (lecturer).
+		"""
+
+		#check if login_status allows permission
+		if (self.login_status == 1):
+			#read and check if user_id exists
+			with open('users.csv', 'r') as csvfile:
+				rdr = csv.reader(csvfile)
+				for row in rdr:
+					if (user_id == row[0]):
+						#user_id exists in the file so return false
+						return False
+			csvfile.close()
+
+			#add user_id, password and permission status to file
+			with open('users.csv', 'w', newline='') as csvfile:
+				new_user = [user_id, password, permission]
+				writer = csv.writer(csvfile, delimiter=',')
+				csvfile.writerows(new_user)
+			csvfile.close()
+			return True
+		else:
+			return False
+
